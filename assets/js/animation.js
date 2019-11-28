418149a5d86c326442491e5a450478c1d17227de
Emilio Cantón <eecanton@gmail.com>
2019-11-28 15:49:00 -0600

    Finish main

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,23 +1,43 @@
-class Ball {
-	draw() {
-		ellipse(mouseX, mouseY, 100);
-	}
-}
+// window.addEventListener('DOMContentLoaded', () => {
+	const iframe = document.querySelector('div[name="infogram"]');
+	console.log(iframe);
+	iframe.addEventListener('load', () => {
+			const button = this.contentWindow.document.querySelector('button.ig-share-button.fullWidth');
+			const logo = this.contentWindow.document.querySelector('img[alt="Infogram logo"');
+			const infolink = this.contentWindow.document
+				.querySelector(
+					'a[href="https://infogram.com/ec688f8f-87c1-4d21-b710-21d6db278f0e"]'
+				)
+				;
+			const infolink2 = this.contentWindow.document.querySelector('a[href="https://infogram.com"]');
 
-windowResized = () => {
-	resizeCanvas(windowWidth, windowHeight);
-}
+			console.log(button);
+			console.log(logo);
+			console.log(infolink);
+			console.log(infolink2);
+	});
+	
+// })
+// class Ball {
+// 	draw() {
+// 		ellipse(mouseX, mouseY, 100);
+// 	}
+// }
 
-let b;
+// windowResized = () => {
+// 	resizeCanvas(windowWidth, windowHeight);
+// }
 
-setup = () => {
-	canvas = createCanvas(windowWidth, windowHeight);
-	canvas.style('z-index', '1');
-	canvas.parent('two');
-	b = new Ball();
-}
+// let b;
 
-draw = () => {
-	background(0);
-	b.draw();
-}
+// setup = () => {
+// 	canvas = createCanvas(windowWidth, windowHeight);
+// 	canvas.style('z-index', '1');
+// 	canvas.parent('two');
+// 	b = new Ball();
+// }
+
+// draw = () => {
+// 	background(0);
+// 	b.draw();
+// }
